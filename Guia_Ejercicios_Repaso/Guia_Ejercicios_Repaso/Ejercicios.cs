93ac9a01b87bd71bf2f26112faec44d6f6ec5ad3
voriana <49135736+voriana@users.noreply.github.com>
2021-08-27 21:31:43 -0300

    Compartir
    Ej01 y Ej02

--- Guia_Ejercicios_Repaso/Guia_Ejercicios_Repaso/Ejercicios.cs
+++ Guia_Ejercicios_Repaso/Guia_Ejercicios_Repaso/Ejercicios.cs
@@ -8,5 +8,101 @@ namespace Guia_Ejercicios_Repaso
 {
     class Ejercicios
     {
+        public void ej01()
+        {
+
+            string registro = "95490443";
+            int hasta = Convert.ToInt32(registro.Substring(5));
+
+            for (int i = 1; i <= hasta; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine(i + "- Foo Bar");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(i + "- Foo");
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine(i + "- Bar");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+
+            }
+
+        }
+
+        public void Ej02()
+        {
+            int contador = 0;
+            int desde = 1;
+            string registro = "95490443";
+            string reg = registro.Substring(5);
+            int hasta = Convert.ToInt32(reg);
+            bool flag = false;
+
+            while (desde <= hasta)
+            {
+                for (int i = 1; i <= desde; i++)
+                {
+                    if (desde % i == 0)
+                    {
+                        contador++;
+
+                    }
+                    if (desde % 3 == 0)
+                    {
+                        flag = true;
+                    }
+                    else
+                    {
+                        flag = false;
+                    }
+
+                }
+
+                if (contador <= 2)
+                {
+                    if (flag)
+                    {
+
+                        Console.WriteLine(desde + "- Es primo y divisible por 3");
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(desde + "- Es primo");
+                    }
+                }
+
+                else if (flag)
+                {
+                    Console.WriteLine(desde + "- Es divisible por 3");
+                }
+                else
+                {
+                    Console.WriteLine(desde + "- ");
+                }
+
+                contador = 0;
+                desde++;
+
+
+
+            }
+
+
+
+
+        }
     }
 }
